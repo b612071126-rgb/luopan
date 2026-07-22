2ce9c0d554456b17b48433020004b6bd9a20eaa5
b612071126-rgb <b612071126@gmail.com>
2026-07-22 13:57:08 +0800

    更新 app.js

--- app.js
+++ app.js
@@ -40,28 +40,76 @@ function getDirection(angle){
 
 
 
+// 1. 找到HTML里的东西
+
+const pointer = document.getElementById("pointer");
+
+const degreeText = document.getElementById("degree");
+
+const directionText = document.getElementById("direction");
+
+
+// 2. 定义变量
+
+let angle = 0;
+
+
+// 3. 方向转换函数（放这里）
+
+function getDirection(angle){
+
+    const directions = [
+        "北",
+        "东北",
+        "东",
+        "东南",
+        "南",
+        "西南",
+        "西",
+        "西北"
+    ];
+
+    // 每45度一个方向
+    let index = Math.round(angle / 45);
+
+    // 防止360度超出数组
+    if(index === 8){
+        index = 0;
+    }
+
+    return directions[index];
+}
+
+
+
 window.addEventListener(
 "deviceorientation",
 function(event){
 
-let heading = event.alpha;
+let heading;
 
 
-// 保存真实角度
-angle = normalizeAngle(heading);
+// iPhone
+if(event.webkitCompassHeading){
+    heading = event.webkitCompassHeading;
+}
+// Android
+else{
+    heading = event.alpha;
+}
 
 
-// 指针旋转
+angle = Math.round(heading);
+
+
 pointer.style.transform =
 `rotate(${-angle}deg)`;
 
 
-// 显示角度
 degreeText.innerHTML =
 "方位角：" + angle + "°";
 
 
-// 显示方向
 directionText.innerHTML =
 "当前：" + getDirection(angle);
 
